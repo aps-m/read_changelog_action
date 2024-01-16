7645e56c6c1421896845cad37eb0a4126125b2cb
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-01-16 11:32:35 +0300

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
  . /app/app_py_env/bin/activate
 
- CONTENT="$(exec python3 /app/changelog_parser.py @)"
+ CONTENT="$(exec python3 /app/changelog_parser.py $@)"
 
  echo 'CONTENT<<EOF' >> $GITHUB_OUTPUT && echo "$CONTENT" >> $GITHUB_OUTPUT && echo 'EOF' >> $GITHUB_OUTPUT
