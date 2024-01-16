7e70e9241bea85dd38cf9e30c22b1db7ba90e592
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-01-16 11:01:15 +0300

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 
  CONTENT="$(exec python3 changelog_parser.py @)"
 
- echo 'content<<EOF' >> $GITHUB_OUTPUT && echo "$CONTENT" >> $GITHUB_OUTPUT && echo 'EOF' >> $GITHUB_OUTPUT
+ echo 'CONTENT<<EOF' >> $GITHUB_OUTPUT && echo "$CONTENT" >> $GITHUB_OUTPUT && echo 'EOF' >> $GITHUB_OUTPUT
